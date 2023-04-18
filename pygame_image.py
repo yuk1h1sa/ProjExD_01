06317c1d48f561f0542a03452ac4a015dcf766c1
yukihisa <c0b22206093@edu.teu.ac.jp>
2023-04-18 17:42:31 +0900

    演習課題

--- pygame_image.py
+++ pygame_image.py
@@ -6,6 +6,7 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
+    bg_img_r = pg.transform.flip(bg_img,True,False)
     kk_img = pg.image.load("ex01/fig/3.png")
     kk_img = pg.transform.flip(kk_img,True,False)
     kk_img_1 = pg.transform.rotozoom(kk_img,10,1.0)
@@ -19,13 +20,14 @@ def main():
             if event.type == pg.QUIT: return
 
         tmr += 1
-        x = tmr%1600
+        x = tmr%3200
         screen.blit(bg_img, [-x,0])
-        screen.blit(bg_img,[1600-x,0])
-        if tmr%2==0:
+        screen.blit(bg_img_r,[1600-x,0])
+        screen.blit(bg_img,[3200-x,0])
+        if tmr%20<=8:
             screen.blit(kk_imgs[0],[300,200])
-        elif tmr%2==1:
-            screen.blit(kk_imgs[1],[300,200]) 
+        elif tmr%20>=8:
+            screen.blit(kk_imgs[1],[300,200])
         pg.display.update()
         clock.tick(100)
 
